33930c0b5d45be1acf0b31fe354147d4cc332a6c
jiyeon <jy7828@gmail.com>
2022-01-29 17:11:42 +0900

    useSelector, useDispatch

--- src/Cart.js
+++ src/Cart.js
@@ -1,8 +1,12 @@
-import { connect } from 'react-redux';
+import { connect, useSelector, useDispatch } from 'react-redux';
 import React from 'react';
 import { Table } from 'react-bootstrap'
 
 const Cart = (props) => {
+
+  let state = useSelector((state) => state.reducer);
+  let dispatch = useDispatch();
+
   return (
     <div>
       <Table responsive striped bordered hover>
@@ -16,15 +20,15 @@ const Cart = (props) => {
         </thead>
         <tbody>
           {
-            props.state.map((item, index) => {
+            state.map((item, index) => {
               return (
                 <tr key={index}>
                   <td>{item.id}</td>
                   <td>{item.name}</td>
                   <td>{item.quan}</td>
                   <td>
-                    <button onClick={() => { props.dispatch({ type: 'inc' }) }}> + </button>
-                    <button onClick={() => { props.dispatch({ type: 'dec' }) }}> - </button>
+                    <button onClick={() => { dispatch({ type: 'inc' }) }}> + </button>
+                    <button onClick={() => { dispatch({ type: 'dec' }) }}> - </button>
                   </td>
                 </tr>
               )
@@ -36,7 +40,7 @@ const Cart = (props) => {
         props.openAlert === true
           ? (<div>
             <p> 지금 구매하면 신규할인 20% </p>
-            <button onClick={() => { props.dispatch({ type: 'close' }) }}> 닫기 </button>
+            <button onClick={() => { dispatch({ type: 'close' }) }}> 닫기 </button>
           </div>)
           : null
       }
@@ -44,17 +48,17 @@ const Cart = (props) => {
   );
 };
 
-// store안에 있던 데이터를 가져와서 props로 return 해주는 함수 
-function storeToProps(state) {
-  return {
-    // name: state[0].name  // 이렇게 적어줘도 되지만
-    // state // store안의  state를 통째로 다 가져옴 
+// // store안에 있던 데이터를 가져와서 props로 return 해주는 함수 
+// function storeToProps(state) {
+//   return {
+//     // name: state[0].name  // 이렇게 적어줘도 되지만
+//     // state // store안의  state를 통째로 다 가져옴 
 
-    // state가 2개 이상이어서 combineReducers 사용한 경우
-    state: state.reducer,
-    openAlert: state.reducer2,
-  }
-}
+//     // state가 2개 이상이어서 combineReducers 사용한 경우
+//     state: state.reducer,
+//     openAlert: state.reducer2,
+//   }
+// }
 
-// export default Cart;
-export default connect(storeToProps)(Cart)
+export default Cart;
+// export default connect(storeToProps)(Cart)
